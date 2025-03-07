8d7deb5d39f0a3a8a94d222c8c3e0d51324c56a5
yoon20002000 <yoon20002000@naver.com>
2025-03-07 23:53:50 +0900

    Initial commit

---
+++ Assets/Scripts/Seeker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Seeker : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 direction;
+
+    void Update()
+    {
+        transform.localPosition += direction * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 direction;
+    
+    void Update()
+    {
+        transform.localPosition += direction * Time.deltaTime;
+    }
+}
