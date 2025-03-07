14102506dd47366c570261e7b500b467f526a692
yoon20002000 <yoon20002000@naver.com>
2025-03-08 00:26:52 +0900

    Draw Line

---
+++ Assets/Scripts/FindNearest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FindNearest : MonoBehaviour
+{
+    void Update()
+    {
+        foreach (var seekerTransform in Spawner.SeekerTransform)
+        {
+            Vector3 seekerPos = seekerTransform.localPosition;
+            Vector3 nearestTargetPos = default;
+            float nearestDistSq = float.MaxValue;
+            foreach (var targetTransform in Spawner.TargetTransform)
+            {
+                Vector3 offset = targetTransform.localPosition - seekerPos;
+                float distSq = offset.sqrMagnitude;
+                if (distSq < nearestDistSq)
+                {
+                    nearestDistSq = distSq;
+                    nearestTargetPos = targetTransform.localPosition;
+                }
+            }
+
+            Debug.DrawLine(seekerPos, nearestTargetPos);
+        }
+    }
+}
