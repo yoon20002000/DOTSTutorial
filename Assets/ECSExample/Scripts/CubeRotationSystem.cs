7af6c56590cb0ba5bbf58472a5f3fbf51afe233e
yoon20002000 <yoon20002000@naver.com>
2025-03-09 21:06:37 +0900

    Rotate System Add

---
+++ Assets/ECSExample/Scripts/CubeRotationSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct CubeRotationSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var deltaTime = SystemAPI.Time.DeltaTime;
+        // RefRW : Read/ Write 참조, RefRO : Read 참조
+        foreach(var(transform, rotationspeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotationSpeed>>())
+        {
+            var radian = rotationspeed.ValueRO.RadiansPerSecond * deltaTime;
+            transform.ValueRW = transform.ValueRW.RotateY(radian);
+        }
+    }   
+}
